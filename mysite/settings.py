29e2ddb8a3863d1262d5949dab61456d21206833
DESKTOP-FHT2P65\Elena <hellens.rez15@gmail.com>
2022-06-15 15:05:31 +0300

    changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
        'http://localhost:8080',
-       
+       'https://blog-on-vue1.herokuapp.com'
 ]
