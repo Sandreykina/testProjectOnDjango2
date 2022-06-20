e3bef2a00396cd5416944d72581808a66be760f8
DESKTOP-FHT2P65\Elena <hellens.rez15@gmail.com>
2022-06-20 13:54:46 +0300

    unimportant changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'elenasandr.pythonanywhere.com', 'elena5645.pythonanywhere.com']
-=======
+
 ALLOWED_HOSTS = ['127.0.0.1', 'elena5645.pythonanywhere.com']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
