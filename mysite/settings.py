7c09538f695cffbf5ea86e33b7024a814ace9958
DESKTOP-FHT2P65\Elena <hellens.rez15@gmail.com>
2022-06-22 10:53:36 +0300

    add csrf and default rest framework settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,8 +146,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
-    ]
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.BasicAuthentication',
+    #     'rest_framework.authentication.SessionAuthentication'
+    # ]
 }
