bb587c15fcc5503382faaa8ebd2376054ae673f6
DESKTOP-FHT2P65\Elena <hellens.rez15@gmail.com>
2022-06-27 18:02:34 +0300

    edit static settings for production

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -17,9 +17,9 @@ const BundleTracker = require("webpack-bundle-tracker");
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "static/assets/dist/"
+      ? "/assets/dist/"
       : "http://127.0.0.1:8080",
-  outputDir: "../static/assets/dist",
+  outputDir: "../assets/dist",
 
   chainWebpack: config => {
     config.optimization.splitChunks(false)
