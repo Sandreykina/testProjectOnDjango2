88e682ba881556bd037e9f5ebe9789d520a2ef85
DESKTOP-FHT2P65\Elena <hellens.rez15@gmail.com>
2022-06-27 08:58:56 +0300

    edit configfile for production

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/static/dist/' : 'http://127.0.0.1:8080',
-  outputDir: '/static/dist',
+  outputDir: '../static/dist',
   indexPath: '../../blog/templates/base-vue.html', // relative to outputDir!
 
   devServer: {
