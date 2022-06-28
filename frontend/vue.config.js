f38c08cd9aa15b2b0e60ab7eb47006c41dab7154
DESKTOP-FHT2P65\Elena <hellens.rez15@gmail.com>
2022-06-28 22:02:36 +0300

    fix bugs

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -15,11 +15,11 @@
 
 const BundleTracker = require("webpack-bundle-tracker");
 module.exports = {
-  // publicPath:
-  //   process.env.NODE_ENV === "production"
-  //     ? "/static/assets/dist/"
-  //     : "http://127.0.0.1:8080",
-  publicPath: "http://127.0.0.1:8080",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/static/assets/dist/"
+      : "http://127.0.0.1:8080",
+  // publicPath: "http://127.0.0.1:8080",
   outputDir: "../assets/dist",
 
   chainWebpack: config => {
